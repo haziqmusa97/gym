1d68712a4d14b8072eb4d77eb437698ae9fb67f2
Haziq Musa <43289189+haziqmusa97@users.noreply.github.com>
2019-11-29 00:41:47 +0800

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -1,7 +1,7 @@
 <?php
-$dbuser="root";
-$dbpass="";
-$host="localhost";
-$db="gym4";
+$dbuser="stf22xc6hke1jbcf";
+$dbpass="lz430j7qiuhxlnxr";
+$host="tyduzbv3ggpf15sx.cbetxkdyhwsb.us-east-1.rds.amazonaws.com	";
+$db="i9nkpk1xscv30ayg";
 $mysqli =new mysqli($host,$dbuser, $dbpass, $db);
 ?>
