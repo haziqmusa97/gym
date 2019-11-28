779d378ebea33247ebc8333a1b2bb1a95a6e87e5
Haziq Musa <43289189+haziqmusa97@users.noreply.github.com>
2019-11-29 00:42:49 +0800

    Update pdoconfig.php

--- includes/pdoconfig.php
+++ includes/pdoconfig.php
@@ -1,8 +1,8 @@
 <?php
-$DB_host = "localhost";
-$DB_user = "root";
-$DB_pass = "";
-$DB_name = "gym4";
+$DB_host = "tyduzbv3ggpf15sx.cbetxkdyhwsb.us-east-1.rds.amazonaws.com	";
+$DB_user = "stf22xc6hke1jbcf";
+$DB_pass = "lz430j7qiuhxlnxr";
+$DB_name = "i9nkpk1xscv30ayg";
 try
 {
  $DB_con = new PDO("mysql:host={$DB_host};dbname={$DB_name}",$DB_user,$DB_pass);
